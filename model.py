6680a6d1ca30523958b3df779f53a3f6c7bd6d90
YasmineMellah <57048633+YasmineMellah@users.noreply.github.com>
2026-08-04 19:03:43 +0100

    Step 15: binary_cross_entropy

--- model.py
+++ model.py
@@ -142,8 +142,12 @@ def logistic_predict_proba(X: np.ndarray, w: np.ndarray, b: float) -> np.ndarray
     probs = np.dot(X, w) + b
     return sigmoid(probs)
 
-# Step 15 - binary_cross_entropy (not yet solved)
-# TODO: implement
+# Step 15 - binary_cross_entropy
+def binary_cross_entropy(y_true: np.ndarray, y_prob: np.ndarray, w: np.ndarray, l2_lambda: float) -> float:
+
+    binary_cross_entropy_error = -(y_true * np.log(y_prob) + (1 - y_true) * np.log(1 - y_prob))
+    
+    return np.mean(binary_cross_entropy_error) +  (l2_lambda * np.sum(w**2)) / 2
 
 # Step 16 - logistic_gradients (not yet solved)
 # TODO: implement
